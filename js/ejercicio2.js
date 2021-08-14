eb16746346d23fcb49d12392976019b597cffd2f
Andersson Suarez <anderssonsuarez35@gmail.com>
2021-08-13 19:45:17 -0500

    Ejercicio2

---
+++ js/ejercicio2.js
@@ -0,0 +1,31 @@
+var ejercicio2 = new Vue({
+    el: "#ejercicio2",
+
+    data: {
+        numcopias: null,
+        valparcial: null,
+        valortot: null,
+        error: null
+    },
+
+    methods: {
+        total(){
+            if (this.numcopias >= 1 && this.numcopias <= 50)
+            {
+                this.valparcial = 50;
+                this.valortot = this.numcopias * this.valparcial;
+            }else if (this.numcopias > 50)
+            {
+                this.valparcial = 30;
+                this.valortot = this.numcopias * this.valparcial;
+            }else
+            {
+                this.error = "Ingresó una cantidad negativa o cero, inténtelo de nuevo"
+            }
+        }
+
+    }
+}
+
+
+)
