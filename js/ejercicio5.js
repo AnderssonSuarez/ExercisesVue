1c80fe84e77476a10e1b2c78fef1bb5c55ce4520
Andersson Suarez <anderssonsuarez35@gmail.com>
2021-08-24 11:52:22 -0500

    Ejercicio 3

---
+++ js/ejercicio5.js
@@ -0,0 +1,21 @@
+var ejercicio5 = new Vue({
+    el: "#ejercicio5",
+
+    data: {
+        comjuan: null,
+        comvic: null,
+        nombre: null,
+        preres: null, 
+        precer: null, 
+        cantcar: null,
+        cantcer: null,
+        total: null,
+        error: null
+    }, 
+
+    methods: {
+        totalp(){
+            this.comjuan = 
+        }
+    }
+})
