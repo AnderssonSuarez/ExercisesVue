21a64f7e8a8040246c86141e9ffc86bc262be102
Andersson Suarez <anderssonsuarez35@gmail.com>
2021-08-25 12:35:28 -0500

    Ejercicio arrays

---
+++ js/array.js
@@ -0,0 +1,13 @@
+var array = new Vue({
+    el: "#array",
+    data: {
+        //Se define un vector o arreglo
+        arrayFrutas:['Pera', 'Piña', 'Manzana', 'Lulo'],  
+        valor: "",
+        arrayColores:['Amarillo', 'Azul', 'Rojo', 'Verde'],
+        cont: ""
+    },
+    methods:{
+
+    }
+})
