a3e11b9a4c8e8e1c04742839dc092dbd438d0963
Andersson Suarez <anderssonsuarez35@gmail.com>
2021-09-08 13:00:58 -0500

    actualizacion ejercicio factura

--- js/ejerFactura.js
+++ js/ejerFactura.js
@@ -6,6 +6,16 @@ var ejerFactura = new Vue({
         opcValor: "0",
         cantidad: null,
         prMetro: 0,
+        edo: true,
+        promEstra1: 0,
+        promEstra2: 0,
+        promEstra3: 0,
+        acuEstra1: 0,
+        contEstra1: 0,
+        acuEstra2: 0,
+        contEstra2: 0,
+        acuEstra3: 0,
+        contEstra3: 0,
         cargoFijo: 0,
         arrayValores: []
     },
@@ -27,10 +37,46 @@ var ejerFactura = new Vue({
         },
         tPagar(){
             var total = this.cargoFijo - (this.cargoFijo * 0.4) + this.cantidad * this.prMetro;
+            this.promMetros();
             this.regFactura(total);
         },
         regFactura(total){
             this.arrayValores.push({Cliente: this.nombre, Estrato: this.estrato, CFijo: this.cargoFijo, Precio: this.prMetro, Cantidad: this.cantidad, Total: total}) 
+        },
+        promMetros(){
+            if(this.estrato == "Estrato 1"){
+                this.acuEstra1 += this.cantidad;
+                this.contEstra1 ++;
+            }else if (this.estrato == "Estrato 2"){
+                this.acuEstra2 += this.cantidad;
+                this.contEstra2 ++;
+            }if (this.estrato == "Estrato 3"){
+                this.acuEstra3 += this.cantidad;
+                this.contEstra3 ++;
+            }
+        },
+        isEdoFalse(){
+            this.edo = false;
+            this.promEstra1 = this.acuEstra1 / this.contEstra1;
+        },
+        isEdoTrue(){
+            this.edo = true;
+        }
+    },
+    computed:{
+        totalPagar: function (){
+            var resultado = 0;
+            for (var i = 0; i < this.arrayValores.length; i++){
+                resultado += this.arrayValores[i].Total;
+            } 
+            return resultado;
+        },
+        totalCantidad: function (){
+            var resultado = 0;
+            for (var i = 0; i < this.arrayValores.length; i++){
+                resultado += this.arrayValores[i].Cantidad;
+            } 
+            return resultado;
         }
     }
 })
